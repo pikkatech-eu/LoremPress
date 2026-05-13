167672563686b3c68b4c28521f901e3fb30642c5
Stanislav <stanislav@pikkatech.eu>
2026-05-13 14:07:11 +0200

    Code\LoremPress\Codes.cs Version in 1st approx ready to pack

--- Code/LoremPress/Codes.cs
+++ Code/LoremPress/Codes.cs
@@ -35,5 +35,63 @@ namespace LoremPress
 			return $"10.{registrant}/{suffix}";
 
 		}
+
+		/// <summary>
+		/// Generates random ISBN 13.
+		/// </summary>
+		/// <returns></returns>
+		public string Isbn()
+		{
+			int[] digits = new int[13];
+
+			digits[0] = 9;
+			digits[1] = 7;
+			digits[2] = 8;
+
+			for (int i = 3; i < 12; i++)
+				digits[i] = Tools.Randomizer.Next(10);
+
+			int sum = 0;
+
+			for (int i = 0; i < 12; i++)
+			{
+				sum += digits[i] * (i % 2 == 0 ? 1 : 3);
+			}
+
+			digits[12] = (10 - (sum % 10)) % 10;
+
+			return string.Concat(digits);
+
+		}
+
+		public string Issn()
+		{
+			int[] digits = new int[8];
+
+			for (int i = 0; i < 7; i++)
+				digits[i] = Tools.Randomizer.Next(10);
+
+			int sum = 0;
+
+			for (int i = 0; i < 7; i++)
+			{
+				sum += digits[i] * (8 - i);
+			}
+
+			int remainder = sum % 11;
+			int checksum = 11 - remainder;
+
+			string checkChar = checksum switch
+			{
+				10 => "X",
+				11 => "0",
+				_ => checksum.ToString()
+			};
+
+			return
+				$"{digits[0]}{digits[1]}{digits[2]}{digits[3]}-" +
+				$"{digits[4]}{digits[5]}{digits[6]}{checkChar}";
+
+		}
 	}
 }
